750d520076c2db7cc870c83ba54712a6d74f5ce0
Sam Dhillon <sam.dhillon1997@gmail.com>
2020-04-21 14:05:31 +0100

    Create common_data_structures.h

---
+++ Script_organiser/common_headers/common_data_structures.h
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+//used to hold information about each script
+struct Script_info
+{
+	char path[100];
+	int no_args;
+	char description[100];
+	char name[100];
+};
+
+
+
+//script information locations
+//#define REPO "/home/samdeep/Projectv2/scripts_repo/\0"
+//#define SCRIPT_LOG "/home/samdeep/Projectv2/scripts_repo/scripts.txt\0"
+//#define SCHEDULED_SCRIPT_LOG "/home/samdeep/Projectsv2/scripts_repo/scheduled_scripts.txt\0"
+//#define SERVER_SCRIPTS "/home/samdeep/Projectv2/scripts_repo/server_scripts.txt\0"
+
